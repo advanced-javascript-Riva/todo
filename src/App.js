6e5bc6d64e883eb87704a8945b7b3b011dad7a56
RivaD2 <rivkadavidowski@gmail.com>
2020-11-27 16:41:04 -0800

    added hideCompleted toggle switch to header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import TodoConnected from './Components/TodoConnected';
 import { TodoContext } from './TodoContext';
+import Form from 'react-bootstrap/Form';
 
 const App = () => {
-
+ 
   return (
     <div className="App">
       <div className="App-header">
           <div className="homeTitle">Home </div>
+        <div className="hideSwitch">
+          <Form>
+            <Form.Check 
+              type="switch"
+              id="custom-switch"
+              label="Hide Completed"
+            />
+          </Form>
+        </div>
         </div>
        <div className="formAndTodo">
          <TodoContext.Provider value="difficulty"> 
